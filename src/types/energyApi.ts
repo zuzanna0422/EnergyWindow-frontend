7b9e78c1cf4833a82be3004d6dcf00421de84d97
Zuzanna Cholewa <zuzanna.cholewa@gmail.com>
2026-01-16 10:54:51 +0100

    Add TypeScript types for energy API responses

---
+++ src/types/energyApi.ts
@@ -0,0 +1,11 @@
+export type EnergyMixDaily = {
+    date: string,
+    fuel: Record<string, number>,
+    cleanEnergyPercentage: number,
+}
+
+export type ChargingWindowResults = {
+    from: string,
+    to: string,
+    cleanCleanEnergyPerc: number,
+}
