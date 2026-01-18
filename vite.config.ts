60153e0f32057af2e203a5902474a3934d3390d5
Zuzanna Cholewa <zuzanna.cholewa@gmail.com>
2026-01-18 13:55:45 +0100

    Setup vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwind from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwind()],
-})
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+  },
+});
